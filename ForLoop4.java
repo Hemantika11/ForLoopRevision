6ef3aafa6a89da3cecd212e67653b34798665f0c
Hemantika <hemarpatel@yahoo.co.uk>
2023-01-15 17:25:19 +0000

    ForLoopConcept

---
+++ ForLoop4.java
@@ -0,0 +1,13 @@
+package H;
+
+public class ForLoop4
+{
+    public static void main(String[] args)
+    {
+        for(int i=10; i>0; i--)
+        {
+            System.out.println(i);
+        }
+    }
+
+}
---
+++ ForLoop6.java
@@ -0,0 +1,16 @@
+package H;
+
+public class ForLoop6
+{
+    public static void main(String[] args)
+    {
+        for(int i=5; i>=0; i--)
+        {
+            for(int k=5; k>=0; k--)
+            {
+                System.out.print(k+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
